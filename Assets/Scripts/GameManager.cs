0af2a2bc9e74e3c1742472e27440e4be2c975661
20140520 <ahacl1479@naver.com>
2017-11-21 11:30:03 +0900

    Unity Start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	private void Start () {
+        BeginGame ();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+	}
+
+    private void BeginGame() { }
+
+    private void RestartGame() { }
+}
